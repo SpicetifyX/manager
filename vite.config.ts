a9b000dde67148c6802ebf59cbd80f97d1ae0900
Jeffrey Reij <contact@jreij.info>
2026-02-16 10:53:25 +0100

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import electron from "vite-plugin-electron/simple";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    electron({
+      main: {
+        entry: "electron/main.ts",
+      },
+      preload: {
+        input: path.join(__dirname, "electron/preload.ts"),
+      },
+      renderer:
+        process.env.NODE_ENV === "test"
+          ? // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
+            undefined
+          : {},
+    }),
+  ],
+});
