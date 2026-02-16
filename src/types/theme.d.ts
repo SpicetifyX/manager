122a174bfffc5d48eff82fa5e4c95399a4de7024
Jeffrey Reij <contact@jreij.info>
2026-02-16 10:55:20 +0100

    Create theme.d.ts

---
+++ src/types/theme.d.ts
@@ -0,0 +1,16 @@
+export type ThemeManifestEntry = {
+  name: string;
+  description: string;
+  usercss?: string;
+  schemes?: string[];
+  include?: string[];
+  preview?: string;
+  authors?: { name: string; url?: string }[];
+  tags?: string[];
+};
+
+export type ThemeInfo = {
+  id: string;
+  isActive: boolean;
+  isBundled?: boolean;
+} & ThemeManifestEntry;
