b598c43b7eff75efbc8db2968230cf2793839f7e
Jeffrey Reij <contact@jreij.info>
2026-02-19 05:17:46 +0100

    Delete electron-env.d.ts

--- electron/electron-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite-plugin-electron/electron-env" />
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    APP_ROOT: string;
-    APP_PATH: string;
-    VITE_PUBLIC: string;
-  }
-}
