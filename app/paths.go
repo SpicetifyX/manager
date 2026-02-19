37fa060b677acf5be124ad88050e7632ef6d2e4d
Jeffrey Reij <contact@jreij.info>
2026-02-19 06:03:22 +0100

    Create paths.go

---
+++ app/paths.go
@@ -0,0 +1,52 @@
+package app
+
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func getSpicetifyxDir() string {
+	home, _ := os.UserHomeDir()
+	return filepath.Join(home, ".spicetifyx")
+}
+
+func getSpicetifyExec() string {
+	dir := getSpicetifyxDir()
+	if runtime.GOOS == "windows" {
+		return filepath.Join(dir, "spicetify.exe")
+	}
+	return filepath.Join(dir, "spicetify")
+}
+
+func getSpicetifyConfigDir() string {
+	if runtime.GOOS == "windows" {
+		return filepath.Join(os.Getenv("APPDATA"), "Spicetify")
+	}
+	home, _ := os.UserHomeDir()
+	return filepath.Join(home, ".config", "spicetify")
+}
+
+func getConfigFilePath() string {
+	return filepath.Join(getSpicetifyConfigDir(), "config-xpui.ini")
+}
+
+func getExtensionsDir() string {
+	return filepath.Join(getSpicetifyConfigDir(), "Extensions")
+}
+
+func getCustomAppsDir() string {
+	return filepath.Join(getSpicetifyConfigDir(), "CustomApps")
+}
+
+func getThemesDir() string {
+	return filepath.Join(getSpicetifyConfigDir(), "Themes")
+}
+
+func getSettingsPath() string {
+	return filepath.Join(getSpicetifyxDir(), "settings.json")
+}
+
+func getAppPath() string {
+	return getSpicetifyConfigDir()
+}
