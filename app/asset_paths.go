ac8b712f7fbff3f75ad8c127247e2cc0b9ee0554
Jeffrey Reij <contact@jreij.info>
2026-02-19 06:41:16 +0100

    Update asset_paths.go

--- app/asset_paths.go
+++ app/asset_paths.go
@@ -1,6 +1,5 @@
 package app
 
-// GetAddonAssetPath converts a relative addon asset path to a URL
 func (a *App) GetAddonAssetPath(assetPath string) string {
 	if isHTTP(assetPath) {
 		return assetPath
@@ -8,7 +7,6 @@ func (a *App) GetAddonAssetPath(assetPath string) string {
 	return "/addon-asset/" + toSlash(assetPath)
 }
 
-// GetThemeAssetPath converts a relative theme asset path to a URL
 func (a *App) GetThemeAssetPath(assetPath string) string {
 	if isHTTP(assetPath) {
 		return assetPath
