1f5f90ef6584c162f654d6d6735350cd9a244087
Jeffrey Reij <contact@jreij.info>
2026-02-19 06:03:06 +0100

    Create app.go

---
+++ app/app.go
@@ -0,0 +1,61 @@
+package app
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+type App struct {
+	ctx          context.Context
+	discord      *DiscordRPC
+	rpcStart     int64
+	rpcConnected bool
+	closeToTray  bool
+	AssetHandler http.Handler
+}
+
+func  New() *App {
+	return &App{
+		AssetHandler: newAssetHandler(),
+	}
+}
+
+func (a *App) Startup(ctx context.Context) {
+	a.ctx = ctx
+	a.discord = NewDiscordRPC("1470628543938433034")
+	a.rpcStart = currentTimeMillis()
+
+	settings, err := ReadSettings()
+	if err == nil {
+		a.closeToTray = settings.CloseToTray
+		if settings.DiscordRpc {
+			go func() {
+				if err := a.discord.Connect(); err == nil {
+					a.rpcConnected = true
+					_ = a.discord.SetActivity(Activity{
+						Name:      "SpicetifyX Manager",
+						Details:   "Viewing Dashboard",
+						CreatedAt: a.rpcStart,
+						Type:      0,
+					})
+				}
+			}()
+		}
+	}
+}
+
+func (a *App) Shutdown(ctx context.Context) {
+	if a.rpcConnected {
+		a.discord.Close()
+	}
+}
+
+func (a *App) WindowMinimize() {
+	runtime.WindowMinimise(a.ctx)
+}
+
+func (a *App) WindowClose() {
+	runtime.Quit(a.ctx)
+}
