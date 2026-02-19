b4d70ada9fc42cd06123cbe4ab374e934ff3ff59
Jeffrey Reij <contact@jreij.info>
2026-02-19 05:18:12 +0100

    Delete install-spicetify-binary.ts

--- electron/ipc/install-spicetify-binary.ts
+++
@@ -1,8 +0,0 @@
-import { ipcMain } from "electron";
-import { installSpicetify } from "../utils/installSpicetify";
-import { getSpicetifyxDir } from "../utils/paths";
-
-ipcMain.handle("install-spicetify-binary", async () => {
-  const spicetifyxDir = getSpicetifyxDir();
-  await installSpicetify(spicetifyxDir);
-});
