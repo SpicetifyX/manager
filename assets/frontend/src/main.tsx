c15c5b7ff27071ce1cfdb276949f91361049ea24
Jeffrey Reij <contact@jreij.info>
2026-02-19 06:04:06 +0100

    Create main.tsx

---
+++ assets/frontend/src/main.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter, Routes, Route, redirect } from "react-router-dom";
+import DashboardPage from "./pages/dashboard";
+import Titlebar from "./components/Titlebar";
+import InstallPage from "./pages/install";
+import * as backend from "../wailsjs/go/app/App";
+import "./style.css";
+import ExtensionsPage from "./pages/extensions";
+import ThemesPage from "./pages/themes";
+import AppsPage from "./pages/apps";
+import SettingsPage from "./pages/settings";
+
+function App() {
+  useEffect(() => {
+    (async () => {
+      const resp = await backend.CheckInstallation();
+
+      if (!resp.patched || !resp.spicetify) {
+        redirect("/install");
+      }
+    })();
+  }, []);
+
+  return (
+    <HashRouter basename="/">
+      <div className="flex h-screen w-screen flex-col overflow-hidden bg-[#171b20]">
+        <Titlebar />
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/install" element={<InstallPage />} />
+          <Route path="/extensions" element={<ExtensionsPage />} />
+          <Route path="/themes" element={<ThemesPage />} />
+          <Route path="/apps" element={<AppsPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
