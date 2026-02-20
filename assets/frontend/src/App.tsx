8606830b80ac93fc38da037d26cb9343780232eb
Jeffrey Reij <contact@jreij.info>
2026-02-20 19:36:06 +0100

    Update App.tsx

--- assets/frontend/src/App.tsx
+++ assets/frontend/src/App.tsx
@@ -181,10 +181,10 @@ export default function App() {
                 onClick={installSpicetify}
                 disabled={installing || installCompleted}
                 className={`flex items-center gap-2 rounded px-5 py-2.5 text-sm font-semibold whitespace-nowrap transition-all duration-200 ${installing
-                    ? "cursor-not-allowed bg-[#a02950] text-white"
-                    : installCompleted
-                      ? "cursor-not-allowed bg-[#2a2a2a] text-white"
-                      : "bg-[#d63c6a] text-white hover:bg-[#c52c5a] active:bg-[#b51c4a]"
+                  ? "cursor-not-allowed bg-[#a02950] text-white"
+                  : installCompleted
+                    ? "cursor-not-allowed bg-[#2a2a2a] text-white"
+                    : "bg-[#d63c6a] text-white hover:bg-[#c52c5a] active:bg-[#b51c4a]"
                   }`}
               >
                 <FaDownload />
