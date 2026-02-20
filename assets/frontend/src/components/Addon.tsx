9d6ae7010ae4f5f7ba47f9d87fe38a8afab5ff45
Jeffrey Reij <contact@jreij.info>
2026-02-20 19:45:30 +0100

    Update Addon.tsx

--- assets/frontend/src/components/Addon.tsx
+++ assets/frontend/src/components/Addon.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Spinner from "./Spinner";
 import { FaTrash, FaInfoCircle } from "react-icons/fa";
 import AddonInfoModal, { AddonInfoData } from "./AddonInfoModal";
