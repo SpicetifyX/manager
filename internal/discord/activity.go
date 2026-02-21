3f28805015b3c8a08d84c3ce3c5a7d8b28149a03
Jort <stormkoning00@gmail.com>
2026-02-21 18:05:24 +0100

    Update activity.go

--- internal/discord/activity.go
+++ internal/discord/activity.go
@@ -6,30 +6,48 @@ import (
 	"os"
 )
 
-func (d *DiscordRPC) SetActivity(activity Activity) error {
+func (d *DiscordRPC) setActivity(activity Activity) error {
 	if !d.connected || d.conn == nil {
 		return fmt.Errorf("not connected")
+	}
+
+	activityMap := map[string]interface{}{
+		"details": activity.Details,
+		"timestamps": map[string]interface{}{
+			"start": activity.CreatedAt / 1000,
+		},
+		"type": activity.Type,
+	}
+	if activity.State != "" {
+		activityMap["state"] = activity.State
+	}
+
+	assets := map[string]interface{}{}
+	if activity.LargeImage != "" {
+		assets["large_image"] = activity.LargeImage
+	}
+	if activity.LargeText != "" {
+		assets["large_text"] = activity.LargeText
+	}
+	if activity.SmallImage != "" {
+		assets["small_image"] = activity.SmallImage
+	}
+	if activity.SmallText != "" {
+		assets["small_text"] = activity.SmallText
+	}
+	if len(assets) > 0 {
+		activityMap["assets"] = assets
 	}
 
 	nonce := fmt.Sprintf("%d", rand.Int63())
 	frame, err := d.encode(1, map[string]interface{}{
 		"cmd": "SET_ACTIVITY",
 		"args": map[string]interface{}{
-			"pid": os.Getpid(),
-			"activity": map[string]interface{}{
-				"name":    activity.Name,
-				"details": activity.Details,
-				"timestamps": map[string]interface{}{
-					"start": activity.CreatedAt / 1000,
-				},
-				"type": activity.Type,
-			},
+			"pid":      os.Getpid(),
+			"activity": activityMap,
 		},
 		"nonce": nonce,
 	})
-	if err != nil {
-		return err
-	}
 	_, err = d.conn.Write(frame)
 	return err
 }
