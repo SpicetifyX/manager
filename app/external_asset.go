410e0c49dea394aed676bf5650ce4f03a39a7461
Jeffrey Reij <contact@jreij.info>
2026-02-28 14:27:46 +0100

    Update external_asset.go

--- app/external_asset.go
+++ app/external_asset.go
@@ -6,18 +6,36 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 func (a *App) GetExternalImageBase64(imageSrc string) string {
-	log.Println(imageSrc)
+	// Normalize GitHub URLs to raw.githubusercontent.com
+	// github.com/{user}/{repo}/blob/{branch}/{path} -> raw.githubusercontent.com/{user}/{repo}/{branch}/{path}
+	// github.com/{user}/{repo}/raw/{branch}/{path} -> raw.githubusercontent.com/{user}/{repo}/{branch}/{path}
+
+	cleanSrc := strings.Split(imageSrc, "?")[0]
+
+	githubBlobRegex := regexp.MustCompile(`https?://github\.com/([^/]+)/([^/]+)/blob/([^/]+)/(.+)`)
+	if match := githubBlobRegex.FindStringSubmatch(cleanSrc); match != nil {
+		cleanSrc = fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/%s/%s", match[1], match[2], match[3], match[4])
+	}
+
+	githubRawRegex := regexp.MustCompile(`https?://github\.com/([^/]+)/([^/]+)/raw/([^/]+)/(.+)`)
+	if match := githubRawRegex.FindStringSubmatch(cleanSrc); match != nil {
+		cleanSrc = fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/%s/%s", match[1], match[2], match[3], match[4])
+	}
+
+	log.Println("Fetching external image:", cleanSrc)
 
 	client := http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
+			return nil // Allow redirects
 		},
 	}
 
-	req, err := http.NewRequest("GET", imageSrc, nil)
+	req, err := http.NewRequest("GET", cleanSrc, nil)
 	if err != nil {
 		log.Println(err)
 		return ""
@@ -28,19 +46,35 @@ func (a *App) GetExternalImageBase64(imageSrc string) string {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Println(err)
+		log.Println("[GetExternalImageBase64] Request failed:", err)
 		return ""
 	}
 
 	defer resp.Body.Close()
 
-	content, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println(err)
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("[GetExternalImageBase64] Unexpected status code %d for %s", resp.StatusCode, cleanSrc)
 		return ""
 	}
 
-	log.Println(imageSrc, len(content))
+	contentType := resp.Header.Get("Content-Type")
+	if !strings.HasPrefix(contentType, "image/") {
+		log.Printf("[GetExternalImageBase64] Invalid content type %s for %s", contentType, cleanSrc)
+		return ""
+	}
 
-	return fmt.Sprintf("data:image/png;base64,%s", base64.StdEncoding.EncodeToString(content))
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("[GetExternalImageBase64] Read failed:", err)
+		return ""
+	}
+
+	if len(content) == 0 {
+		log.Println("[GetExternalImageBase64] Empty content for:", cleanSrc)
+		return ""
+	}
+
+	log.Printf("[GetExternalImageBase64] Fetched %d bytes from %s (Type: %s)", len(content), cleanSrc, contentType)
+
+	return fmt.Sprintf("data:%s;base64,%s", contentType, base64.StdEncoding.EncodeToString(content))
 }
